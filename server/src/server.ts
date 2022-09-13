b02753934b6023cded965ab183b5070d5acd9f3f
Evandro Lima <96010876+EvandroLimaBarbosa@users.noreply.github.com>
2022-09-13 00:47:29 -0300

    att

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,6 +2,8 @@ import express, { response } from "express";
 
 const app = express();
 
+// localhost:3333/ads
+
 app.get("/ads", (request, response) => {
   return response.json([
     { id: 1, name: "Anuncio 1" },
