e4d5b09bf30b4100de037de9250fa29b0c40fcef
Evandro Lima <96010876+EvandroLimaBarbosa@users.noreply.github.com>
2022-09-13 22:40:03 -0300

    Update App.tsx

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,20 +1,43 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, TouchableOpacity, TouchableWithoutFeedback, ButtonProps } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text style={styles.title}>Se Liga!!</Text>
+      <Button title="Não Aperte!!"/>
       <StatusBar style="auto" />
     </View>
+  );
+}
+
+interface ButtonProps{
+  title: string;
+}
+
+
+function Button(props: ButtonProps) {
+  return (
+    <TouchableOpacity >
+      <Text style={styles.button}>{props.title}</Text>
+    </TouchableOpacity>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#3f3f3f",
+    alignItems: "center",
+    justifyContent: "center",
   },
+  title: {
+    color: "#ac0000",
+    fontSize: 50,
+  },
+  button: {
+    backgroundColor: "blue",
+    margin: 10,
+    padding: 5
+  }
 });
