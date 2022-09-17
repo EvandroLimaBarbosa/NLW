d6c3841cb39cbc8a5c9d9bc6c76724c4d0482536
Evandro Lima <96010876+EvandroLimaBarbosa@users.noreply.github.com>
2022-09-17 03:21:32 -0300

    Revert "Update server.ts"
    This reverts commit 50b7e7d03f6874d6e847708f5d3d07964244aadc.

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,39 +1,16 @@
-import express, { request, response } from "express";
+import express, { response } from "express";
 
 const app = express();
 
-// primeiro entre na pasta server e
-// depois digite no terminal
-// npm run dev
-
-// colocar no hoppscotch
 // localhost:3333/ads
 
-app.get('/games', (request, response) => {
-  return response.json([]);
-});
-
-app.post('/ads', (request, response) => {
-  return response.status(201).json(['']);
-});
-
-
-app.get("/games/:id/ads", (request, response) => {
-  //const gameId = request.params.id;
-
+app.get("/ads", (request, response) => {
   return response.json([
     { id: 1, name: "Anuncio 1" },
     { id: 2, name: "Anuncio 2" },
     { id: 3, name: "Anuncio 3" },
     { id: 4, name: "Anuncio 4" },
     { id: 5, name: "Anuncio 5" },
-  ]);
-});
-
-app.get("/ads/:id/discord", (request, response) => {
-  //const adId = request.params.id;
-
-  return response.json([
   ]);
 });
 
