8cb90b39c3979075d9c46f7e16d066583b6e0cd8
Evandro Lima <96010876+EvandroLimaBarbosa@users.noreply.github.com>
2022-09-29 06:54:06 -0300

    fiz a area do jogo

--- mobile/src/screens/Game/styles.ts
+++ mobile/src/screens/Game/styles.ts
@@ -1,5 +1,31 @@
 import { StyleSheet } from 'react-native';
 
 export const styles = StyleSheet.create({
-  container: {}
+  container: {
+    flex: 1,
+    alignItems: 'center',
+  },
+  header: {
+    width: '100%',
+    flexDirection: 'row',
+    alignItems: 'center',
+    paddingHorizontal: 32,
+    marginTop: 28,
+    justifyContent: 'space-between'
+  },
+  logo: {
+    width: 72,
+    height: 40
+  },
+  right: {
+    width: 30,
+    height: 30
+  },
+  banner: {
+    width: 311,
+    height: 190,
+    borderRadius: 8,
+    marginTop: 32
+  }
+
 });
