3c7c465f0551d4bf6495408cd8672240954c4851
Evandro Lima <96010876+EvandroLimaBarbosa@users.noreply.github.com>
2022-10-07 06:38:26 -0300

    style duo card att

--- mobile/src/components/DuoCard/index.tsx
+++ mobile/src/components/DuoCard/index.tsx
@@ -17,9 +17,10 @@ export interface DuoCardProps {
 
 interface Props {
   data: DuoCardProps;
+  onConnect: () => void;
 }
 
-export function DuoCard({data}: Props) {
+export function DuoCard({data, onConnect}: Props) {
   return (
     <View style={styles.container}>
       <Duoinfo label="Nome" value={data.name} />
@@ -27,7 +28,7 @@ export function DuoCard({data}: Props) {
       <Duoinfo label="Disponibilidade" value={`${data.weekDays.length} dias \u2022 ${data.hourStart} - ${data.hourEnd}`} /> 
       <Duoinfo label="Chamada de áudio?" value={data.useVoiceChannel ? "Sim" : "Não"} colorValue={data.useVoiceChannel ? THEME.COLORS.SUCCESS : THEME.COLORS.ALERT}/>
 
-      <TouchableOpacity style={styles.button}>
+      <TouchableOpacity style={styles.button} onPress={onConnect}>
         <GameController
         color={THEME.COLORS.TEXT}
         size={20}
